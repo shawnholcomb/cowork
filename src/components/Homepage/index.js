76cb658304d1cd2a8a92625a4503dbb5a1f0535e
Shawn Holcomb <sholcmb@gmail.com>
2019-05-08 02:42:37 -0500

    completed landing page and styled navbar

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Homepage.css';
+import NavBar from '../NavBar';
 import HomeHeader from '../HomeHeader';
 import HIWMain from '../HIWMain';
-
+import SignIn from '../SignIn';
+import Footer from '../Footer';
 
 class Homepage extends React.Component {
     state = {
@@ -12,8 +14,11 @@ class Homepage extends React.Component {
     render() {
         return (
             <div>
+                <NavBar />
                 <HomeHeader />
                 <HIWMain />
+                <SignIn />
+                <Footer />
             </div>
         )
     };
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './NavBar.css';
 
+const styles = {
+    h5: {
+        color: '#0078df'
+    },
+    border: {
+        borderBottom: '1px solid #f5f5f5',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.25)'
+    }
+}
 
-const NavBar = props => (
-    <nav>
-        <h1><a href='/'>{props.title}</a></h1>
+const NavBar = () => (
+    <nav style={styles.border}>
+        <div className="nav-logo">
+            <a href='/home'><img alt="devconnect logo" src="images/dclogo-color.png"></img></a>
+        </div>
+        <div className="nav-links">
+            <ul className="nav-list">
+                <li><a href="/"><h5 style={styles.h5}>How it Works</h5></a></li>
+                <li><a href="/"><h5 style={styles.h5}>Sign In</h5></a></li>
+
+            </ul>
+        </div>
     </nav>
 )
 
