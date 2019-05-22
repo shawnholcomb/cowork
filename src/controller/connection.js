a0de45f624cdeef2bd84033400500b1c5b574283
Shawn Holcomb <sholcmb@gmail.com>
2019-05-22 17:47:28 -0500

    setup Heroku server and JawsDB

--- src/controller/connection.js
+++ src/controller/connection.js
@@ -1,14 +1,15 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ft041104',
-    database: 'devconnect'
+    host: "lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    port: 3306,
+    user: "dnpx038dvx1tyhfx	",
+    password: "ako42k94r9vuq2o7",
+    database: "kr1rj7xj7dki7t9a	"
 })
 
 connection.connect(err => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("connected to database.");
 })
 
